b8a19654aeec30ce20f75d4294c0024b9f9853fe
Anirudh Sarma <ani.toon24@gmail.com>
2016-04-07 06:49:44 +0530

    midterm test website for selenium testing

---
+++ test.pl
@@ -0,0 +1,16 @@
+ use WWW::Selenium;
+
+    my $sel = WWW::Selenium->new( host => "localhost",
+                                  port => 4444,
+                                  browser => "*firefox",
+                                  browser_url => "http://www.google.com",
+                                );
+
+    $sel->start;
+    $sel->open("http://www.google.com");
+    $sel->type("q", "hello world");
+    $sel->click("btnG");
+    $sel->wait_for_page_to_load(5000);
+    print $sel->get_title;
+    $sel->stop;
+
